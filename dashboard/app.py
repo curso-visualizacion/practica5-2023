12b02f25cc4dda47741b03ec609e4d47c7b0a954
Diego <diegorodriguezmancini@gmail.com>
2023-04-20 19:30:15 -0400

    punto de partida practica 4

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,9 +1,77 @@
-from dash import Dash, html
+from dash import Dash, Input, Output, dcc, html
+import pandas as pd
+import plotly.express as px
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
-server = app.server
+gapminder = pd.read_csv("datasets/gapminderData2.csv")
+scatter_figure = px.scatter(
+    gapminder[gapminder.year == 2007], x="bornPerwom", y="gdpPercap"
+)
 
-app.layout = html.Div(children="Hello World")
+migrantes = pd.read_excel("datasets/MigrantesChile.xlsx")
+
+
+def plot_heatmap(data: pd.DataFrame, continent):
+    data.loc[:, "total"] = data.loc[:, range(2005, 2016)].sum(axis=1)
+    data2 = data.sort_values("total", ascending=False, inplace=False)
+    data2 = data2[data2.Continent == continent]
+    top5 = data2.iloc[:5]
+    top5.set_index("Country", inplace=True)
+    heatmap = px.imshow(
+        top5[range(2005, 2016)],
+        labels={"x": "Year", "y": "Country", "color": "Migrantes"},
+        x=list(range(2005, 2016)),
+        y=top5.index.values,
+    )
+    return heatmap
+
+
+def continent_options(data):
+    return [
+        {"label": continent, "value": continent}
+        for continent in data.Continent.unique()
+    ]
+
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+app.layout = dbc.Container(
+    children=[
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.H1("titulo"),
+                        dcc.Graph(id="id_scatter", figure=scatter_figure),
+                    ],
+                    width=7,
+                ),
+                dbc.Col(
+                    [
+                        html.H1("Heatmap"),
+                        dbc.Select(
+                            id="select",
+                            options=continent_options(migrantes),
+                            value="América",
+                        ),
+                        dcc.Graph(
+                            id="id_heatmap", figure=plot_heatmap(migrantes, "América")
+                        ),
+                    ]
+                ),
+            ]
+        ),
+    ],
+    fluid=True,
+)
+
+
+@app.callback(
+    Output(component_id="id_heatmap", component_property="figure"),
+    Input(component_id="select", component_property="value"),
+)
+def update_heatmap(continent):
+    return plot_heatmap(migrantes, continent)
 
 
 if __name__ == "__main__":
